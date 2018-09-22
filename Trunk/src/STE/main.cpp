29368100aa3b4dbe551b9e9c4aedf67019e287df
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2018-09-22 00:18:53 -0400

    window creation successful

--- Trunk/src/STE/main.cpp
+++ Trunk/src/STE/main.cpp
@@ -2,8 +2,33 @@
 // Author: Pierce Brooks
 
 #include <STE/Menu.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 int main()
 {
+    sf::RenderWindow* window = new sf::RenderWindow();
+    auto videoModes = sf::VideoMode::getFullscreenModes();
+    // ^ hey C&D, check that one out lol
+    window->create(videoModes[videoModes.size()-1], "Satisfy the Elements", sf::Style::Default);
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::EventType::Closed:
+                window->close();
+                break;
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
+        {
+            window->close();
+            continue;
+        }
+    }
+    delete window;
     return 0;
 }
